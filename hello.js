5cd47dc9c2a0ef3af196436dfe9d63fbd13b3f49
zfpx <zfpx@163.com>
2015-10-20 11:05:13 +0800

    444

---
+++ hello.js
@@ -0,0 +1,11 @@
+console.log('1');
+if(true){
+    console.log(2);
+}
+
+console.log(process.pid);
+
+setInterval(function(){
+    console.log('hello');
+},1000)
+//window.alert('3');
---
+++ world.js
@@ -0,0 +1,8 @@
+console.log('1');
+if(true){
+    console.log(2);
+}
+while(true){
+    
+}
+//window.alert('3');
